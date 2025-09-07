9be5ebefc1c6721f14b1e9a5467e876ef416081d
raunaksharmaq64 <raunaksharma88630mt@gmail.com>
2025-09-07 20:38:09 +0530

    add one method

--- recursion_2.java
+++ recursion_2.java
@@ -1,13 +1,39 @@
 public class recursion_2 {
     public static void main(String[] args) {
- arr(0);
+
+
     };
 
-    static void  arreyMax(int indx){
-        int[]array={222,3332,33,2,12334,222223,22,1223324};
+
+
+    
+    static int array2(int index){
+        int [] arr={223,33,222,333,23,23,88387,23,33};
+        if (index==arr.length) return 0;
         
+    System.out.print(arr[index]+ " , ");
+
+        return array2(index+1);
+
+
+
     }
 
+ 
+
+    static int array(int index){
+        int[]array={223,378,838,3893,777};
+        if(index==array.length){ 
+        return 0 ;
+    };
+    
+        return array[index]+array(index+1);
+        
+        
+        
+
+
+    }
 
     static void arr(int indx){
         int[]array={12,11,43,334,555,3};
